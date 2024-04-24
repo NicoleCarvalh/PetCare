00e8702f4b317c01676bc52b6db1630872b27602
NicoleCarvalh <nicolecarvalho6@gmail.com>
2024-04-24 10:21:13 -0300

    feat: Adding credential verification with API

--- api.py
+++ api.py
@@ -1,7 +1,35 @@
+import os
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def verify_credentials(email, password):
-  url = "https://api-petcare.onrender.com/employees"
+  api_key = os.getenv("API_KEY")
+
+  if not api_key:
+    raise ValueError("API_KEY não encontrada no arquivo .env")
+  
+  url = f"https://api-petcare.onrender.com/employees?auth={api_key}"
+# url = "https://api-petcare.onrender.com/employees"
   payload = {"email": email, "password": password}
   response = requests.post(url, json=payload)
-  return response
+
+  if response.status_code == 200:
+    # Return data 
+    return response.json()
+  else:
+    # Raise exception if there's an error
+    response.raise_for_status()
+
+
+# Example of use
+if __name__ == "__main__":
+  email = "teste@teste.com"
+  password = "123"
+
+  try:
+    data = verify_credentials(email, password)
+    print("Dados do usuário:", data)
+  except Exception as e:
+    print("Erro ao verificar credenciais:", e)
--- main.py
+++ main.py
@@ -2,21 +2,26 @@ from PyQt5.QtWidgets import QApplication, QDialog, QFrame, QLabel, QLineEdit, QM
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon, QFont, QFontDatabase
 from PyQt5.QtCore import pyqtSlot, QFile, QTextStream
+from dotenv import load_dotenv
+import requests
 import sys
+from api import verify_credentials
 
 #TODO design the pages left
+
 class Main(QDialog):
   def __init__(self):
     super(Main, self).__init__()
     
-    # Load the UI file
+    # Load the Login UI file
     uic.loadUi("screens/login.ui", self)
     
     # Set window Icon and Title
     self.setWindowTitle("PetCare Connect")
     self.setWindowIcon(QIcon('resources/images/DesktopIcon.png'))
+    self.login_button.clicked.connect(self.login)
 
-    ####### FONTS ######
+    ################ FONTS ##################
     # Insert the font into the font database
     font_path = 'resources/fonts/Inter/Inter.ttf'
     QFontDatabase.addApplicationFont(font_path)
@@ -29,6 +34,7 @@ class Main(QDialog):
     button_font.setWeight(500)
     link_font = QFont(inter_font_family, 11)
     link_font.setUnderline(True)
+    validation_font = QFont(inter_font_family, 11)
 
     # Change font for specific widgets
     self.greetings.setFont(title_font)  
@@ -37,9 +43,28 @@ class Main(QDialog):
     self.login_button.setFont(button_font)  
     self.forgot_password.setFont(link_font)  
     self.register_label.setFont(link_font)  
+    self.validation_text.setFont(validation_font)
 
-    ####################
-    
+    #########################################
+  
+  def login(self):
+    email = self.email_input.text()
+    password = self.password_input.text()
+
+    if len(email) == 0 or len(password) == 0:
+      self.validation_text.setText("Por favor, preencha todos os campos.")
+      return
+
+    response = verify_credentials(email, password)
+    if response.status_code == 200:
+      self.load_dashboard()
+    else:
+      self.validation_text.setText("E-mail ou senha incorretos.")
+
+  # Load the Dashboard UI file
+  def load_dashboard(self):
+    uic.loadUi("screens/dashboard.ui", self) 
+    self.show()
 
 # Initialize App
 if __name__ == "__main__":
