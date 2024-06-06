e41bade1cb63aaf9dbd81e07d9c7ca30f9058132
NicoleCarvalh <nicolecarvalho6@gmail.com>
2024-06-05 21:18:18 -0300

    design: Saving changes in Dashboard page

--- pages/stats_page.py
+++ pages/stats_page.py
@@ -3,6 +3,8 @@ from PyQt5.QtGui import *
 from backend.routes import format_date, format_value
 from backend.endpoints.sales import get_sales_list
 
+# Consumir API e informar dados nos blocos 
+# Verificar gráficos
 
 def populate_dashboard(self):
   pass
